b0bb1cf41e3923d3fb8539fb51f91a5c31c2c5d0
Delia Elena Barbuta <delia-elena.barbuta@student.tuiasi.ro>
2022-05-04 15:49:23 +0300

    Laboratorul 1 fara punctul 3

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser')
+
+const app = express();
+
+const port = 6789;
+
+// directorul 'views' va conține fișierele .ejs (html + js executat la server)
+app.set('view engine', 'ejs');
+// suport pentru layout-uri - implicit fișierul care reprezintă template-ul site-ului este views/layout.ejs
+app.use(expressLayouts);
+// directorul 'public' va conține toate resursele accesibile direct de către client (e.g., fișiere css, javascript, imagini)
+app.use(express.static('public'))
+// corpul mesajului poate fi interpretat ca json; datele de la formular se găsesc în format json în req.body
+app.use(bodyParser.json());
+// utilizarea unui algoritm de deep parsing care suportă obiecte în obiecte
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// la accesarea din browser adresei http://localhost:6789/ se va returna textul 'Hello World'
+// proprietățile obiectului Request - req - https://expressjs.com/en/api.html#req
+// proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
+app.get('/', (req, res) => res.send('Hello World'));
+
+const listaIntrebari = [
+    {
+        intrebare: 'Întrebarea 1',
+        variante: ['varianta 1', 'varianta 2', 'varianta 3', 'varianta 4'],
+        corect: 0
+    },
+    //...
+];
+// la accesarea din browser adresei http://localhost:6789/chestionar se va apela funcția specificată
+app.get('/chestionar', (req, res) => {
+	// în fișierul views/chestionar.ejs este accesibilă variabila 'intrebari' care conține vectorul de întrebări
+	res.render('chestionar', {intrebari: listaIntrebari});
+});
+
+app.post('/rezultat-chestionar', (req, res) => {
+	console.log(req.body);
+	//res.send("formular: " + JSON.stringify(req.body)
+    var punctaj = 0
+    for(let i in listaIntrebari){
+        if(req.body[`q${i}`] == listaIntrebari[i].corect)
+            punctaj++;
+    }
+    res.render('rezultat-chestionar', {punctaj: punctaj});
+    console.log(punctaj)
+});
+
+app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:`));
