23fbf962f7c5eac8126a28e17dc5c3eee33a5197
Delia Elena Barbuta <delia-elena.barbuta@student.tuiasi.ro>
2022-05-05 14:31:16 +0300

    Lab 10 functionalitati complete

--- app.js
+++ app.js
@@ -21,31 +21,53 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // proprietățile obiectului Request - req - https://expressjs.com/en/api.html#req
 // proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
 app.get('/', (req, res) => res.send('Hello World'));
+const fs = require('fs');
+// let rawdata = fs.readFileSync('intrebari.json');
+// let intrebari = JSON.parse(rawdata);
+fs.readFile('intrebari.json', (err, data) => {
+    if (err) throw err;
+    let intrebari = JSON.parse(data);
+    app.get('/chestionar', (req, res) => {
+        // în fișierul views/chestionar.ejs este accesibilă variabila 'intrebari' care conține vectorul de întrebări
+        res.render('chestionar', {intrebari: intrebari});
+    });
+    app.post('/rezultat-chestionar', (req, res) => {
+        console.log(req.body);
+        //res.send("formular: " + JSON.stringify(req.body)
+        var punctaj = 0
+        for(let i in intrebari){
+            if(req.body[`q${i}`] == intrebari[i].corect)
+                punctaj++;
+        }
+        res.render('rezultat-chestionar', {punctaj: punctaj});
+        console.log(punctaj)
+    });
+});
 
-const listaIntrebari = [
-    {
-        intrebare: 'Întrebarea 1',
-        variante: ['varianta 1', 'varianta 2', 'varianta 3', 'varianta 4'],
-        corect: 0
-    },
-    //...
-];
+// const listaIntrebari = [
+//     {
+//         intrebare: 'Când au devenit alimentele bio cunoscute?', 
+//         variante: ['1900', '1940', '1960', '1970'],
+//         corect: 1
+//     },
+//     {
+//         intrebare: 'Se pot utiliza pesticide în producția alimentelor bio?',
+//         variante: ['Nu', 'Da, dacă acestea nu sunt sintetice', 'Da'],
+//         corect: 1
+//     },
+//     {
+//         intrebare: 'Ce reprezintă efectul Halo?',
+//         variante: ['Efectul de luminare a unei suprafețe', 'Tendința unei persoane de a consumă alimente pe care le-a cunoaște dintr-un context anterior', 'Efectul prin care percepția unei persoane este afectată de credințele sale'],
+//         corect: 2
+//     }
+
+// ];
+
+
 // la accesarea din browser adresei http://localhost:6789/chestionar se va apela funcția specificată
-app.get('/chestionar', (req, res) => {
-	// în fișierul views/chestionar.ejs este accesibilă variabila 'intrebari' care conține vectorul de întrebări
-	res.render('chestionar', {intrebari: listaIntrebari});
-});
-
-app.post('/rezultat-chestionar', (req, res) => {
-	console.log(req.body);
-	//res.send("formular: " + JSON.stringify(req.body)
-    var punctaj = 0
-    for(let i in listaIntrebari){
-        if(req.body[`q${i}`] == listaIntrebari[i].corect)
-            punctaj++;
-    }
-    res.render('rezultat-chestionar', {punctaj: punctaj});
-    console.log(punctaj)
-});
+// app.get('/chestionar', (req, res) => {
+// 	// în fișierul views/chestionar.ejs este accesibilă variabila 'intrebari' care conține vectorul de întrebări
+// 	res.render('chestionar', {intrebari: intrebari});
+// });
 
 app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:`));
