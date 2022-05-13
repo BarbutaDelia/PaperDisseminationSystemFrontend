0be384e29845278c15accdb3cb231ff1c18f7012
Delia Elena Barbuta <delia-elena.barbuta@student.tuiasi.ro>
2022-05-13 12:31:52 +0300

    Laboratorul 11 complet

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser')
 const cookieParser=require('cookie-parser');
+const session = require('express-session');
+// app.use(session({secret: 'ssshhhhh'}));
+var sess;
 
 const app = express();
 
@@ -18,12 +21,13 @@ app.use(bodyParser.json());
 // utilizarea unui algoritm de deep parsing care suportă obiecte în obiecte
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
+app.use(session({secret: 'ssshhhhh'}))
 
 // la accesarea din browser adresei http://localhost:6789/ se va returna textul 'Hello World'
 // proprietățile obiectului Request - req - https://expressjs.com/en/api.html#req
 // proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
-app.get('/', (req, res) => res.render('index', {u: req.cookies.utilizator}));
-const fs = require('fs');
+app.get('/', (req, res) => res.render('index', {u: req.session.username}));
+const fs = require('fs');   
 // let rawdata = fs.readFileSync('intrebari.json');
 // let intrebari = JSON.parse(rawdata);
 fs.readFile('intrebari.json', (err, data) => {
@@ -47,24 +51,62 @@ fs.readFile('intrebari.json', (err, data) => {
     }); 
 });
 app.get('/autentificare', (req, res) => {
-    res.clearCookie("mesajEroare")
-    if(req.cookies.utilizator == null)
-        res.render('autentificare', {m: req.cookies.mesajEroare})
-    res.redirect('/')
-})
-app.post('/verificare-autentificare', (request, response) => {
-    let username = request.body.username;
-	let password = request.body.password;
-    if(username == "delia" && password == "delia"){
-        response.cookie('utilizator', 'delia')
-        response.redirect('/')
+    // res.clearCookie("mesajEroare")
+    // if(req.cookies.utilizator == null)index
+    //     res.render('autentificare', {m: req.cookies.mesajEroare})
+    // res.redirect('/')
+    sess = req.session;
+    if(sess.username != null){
+        res.redirect('/')
     }
     else{
-        response.cookie('mesajEroare', 'Utilizator sau parola gresite!')
-        response.redirect('/autentificare')
+        res.render('autentificare', {e: req.session.errorMsg})
     }
+})
+let rawdata = fs.readFileSync('utilizatori.json');
+let utilizatori = JSON.parse(rawdata);
+
+app.post('/verificare-autentificare', (request, response) => {
+    console.log(utilizatori)
+    let username = request.body.username;
+	let password = request.body.password;
+    for(let i in utilizatori){
+        if(username == utilizatori[i].utilizator && password == utilizatori[i].parola){
+            sess=request.session;
+            sess.username = username;
+            sess.lastName = utilizatori[i].nume;
+            sess.firstName = utilizatori[i].prenume;
+            // response.cookie('utilizator', 'delia')
+            response.redirect('/')
+            return
+        }
+    }
+    sess = request.session
+    sess.errorMsg = "Utilizator sau parola gresite!"
+    // response.cookie('mesajEroare', 'Utilizator sau parola gresite!')
+    response.redirect('/autentificare')
+    // if(username == "delia" && password == "delia"){
+    //     sess=request.session;
+    //     sess.username = username;
+    //     // response.cookie('utilizator', 'delia')
+    //     response.redirect('/')
+    // // }
+    // else{
+    //     sess = request.session
+    //     sess.errorMsg = "Utilizator sau parola gresite!"
+    //     // response.cookie('mesajEroare', 'Utilizator sau parola gresite!')
+    //     response.redirect('/autentificare')
+    // }
 
 });
+app.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if(err) {
+            return console.log(err);
+        }
+        res.redirect('/');
+    });
+})
 
 // const listaIntrebari = [
 //     {
